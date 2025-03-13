842114a4a1affc2cf87b1624055685bbd28059ec
iwimmer <iwimmer@student.ethz.ch>
2025-03-13 18:47:26 +0100

    NextJS setup + heroui/react added

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-neutral-950 h-screen w-full flex justify-center items-center">
+      <h1 className="text-2xl font-semibold">Start Hack 2025</h1>
+    </div>
+  );
+}
