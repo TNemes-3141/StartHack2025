2a65c929485491e4759a7a4a93314a95a7d18d16
Tamas Nemes <nemestomi2@gmail.com>
2025-03-16 23:42:54 +0100

    Implement Supabase connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import { createClient } from "@/lib/supabase/server";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+
   return (
     <div className="bg-neutral-950 h-screen w-full flex justify-center items-center">
       <h1 className="text-2xl font-semibold">Start Hack 2025</h1>
+      { supabase ?
+        <p>Supabase client successfully initialized!</p> :
+        <p>Supabase did not respond yet...</p>
+      }
     </div>
   );
 }
