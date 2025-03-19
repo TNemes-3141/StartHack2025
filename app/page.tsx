aaac61759d32cca472375dc67a2556d524021799
Tamas Nemes <nemestomi2@gmail.com>
2025-03-19 15:35:48 +0100

    Update fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
+import { cn } from "@/lib/utils";
+
 import { createClient } from "@/lib/supabase/server";
 import { ThemeSwitcher } from "./components/ThemeSwitcher";
 import { SupabaseTestButton } from "./components/SupabaseTestButton";
 import MilvusStatus from "./components/MilvusStatus";
+import { secondary_font } from "@/lib/fonts";
 
 export default async function Home() {
   const supabase = await createClient();
 
   return (
     <div className="bg-background h-screen w-full flex flex-col justify-center items-center gap-8">
-      <h1 className="text-2xl font-semibold">Start Hack 2025</h1>
+      <h1 className={cn("text-4xl font-semibold", secondary_font.className)}>Start Hack 2025</h1>
       {supabase ?
         <p>Supabase client successfully initialized!</p> :
         <p>Supabase did not respond yet...</p>
--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,11 +1,22 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local';
 
-export const primary_font = Geist({
+export const primary_font = localFont({
+    src: "../public/Satoshi-Variable.ttf",
+    display: 'swap',
+});
+
+/*Geist({
     variable: "--font-geist-sans",
     subsets: ["latin"],
+});*/
+
+export const secondary_font = localFont({
+    src: "../public/ClashDisplay-Variable.ttf",
+    display: 'swap',
 });
 
-export const secondary_font = Geist_Mono({
+/*Geist_Mono({
     variable: "--font-geist-mono",
     subsets: ["latin"],
-});
+});*/
