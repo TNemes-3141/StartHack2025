01305a3e09e21a0af172567f1f238d77fda505fc
TotallyInformatik <totallyturing@gmail.com>
2025-03-19 21:38:57 +0100

    deleted useless stuff

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,14 @@ import { cn } from "@/lib/utils";
 
 import { createClient } from "@/lib/supabase/server";
 import { ThemeSwitcher } from "./components/ThemeSwitcher";
-import { SupabaseTestButton } from "./components/SupabaseTestButton";
-import MilvusStatus from "./components/MilvusStatus";
 import { secondary_font } from "@/lib/fonts";
 
 export default async function Home() {
-  const supabase = await createClient();
-
-  return (
-    <div className="bg-background h-screen w-full flex flex-col justify-center items-center gap-8">
-      <h1 className={cn("text-4xl font-semibold", secondary_font.className)}>Start Hack 2025</h1>
-      {supabase ?
-        <p>Supabase client successfully initialized!</p> :
-        <p>Supabase did not respond yet...</p>
-      }
+  return <>
+    <header className="p-5 h-14 w-screen flex align-center">
+      <h1 className="h-fit">SIX</h1>
       <ThemeSwitcher />
-      <SupabaseTestButton />
-      <MilvusStatus />
-    </div>
-  );
+    </header>
+  </>
 }
+
