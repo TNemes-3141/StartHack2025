7f9f2324e9049842ebdf42ea21e1bd90e7e08101
TotallyInformatik <totallyturing@gmail.com>
2025-03-19 23:19:08 +0100

    history works for user

--- app/page.tsx
+++ app/page.tsx
@@ -11,16 +11,38 @@ import Image from "next/image";
 import { Button } from "@heroui/button";
 import { Card, CardBody } from "@heroui/react";
 import { History } from "lucide-react"
+import { useRef, useState } from "react";
+
+// chat can we get a pog chat?
+type ChatHistory = {
+  sender: "assistant" | "user",
+  message: string
+}[]
 
 export default function Home() {
+
+  const [showHistory, setShowHistory] = useState<boolean>(true);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [history, setHistory] = useState<ChatHistory>([]);
+
   return <>
     <div className="h-screen w-screen flex">
-      <aside className={cn(styles.history, "w-[400px] bg-secondary-light dark:bg-secondary-dark")}>
+      <aside className={cn(styles.history, "h-full bg-secondary-light dark:bg-secondary-dark", showHistory ? "w-[400px]" : "hidden")}>
+        <h2 className="p-5">Interaction Chat</h2>
+        <main className="w-full flex flex-col p-5 justify-self-end">
+          {
+            history.map((message, idx) => {
+              return <Card className={cn("my-2 p-2 w-fit w-max-[200px] bg-main-light dark:bg-main-dark", message.sender == "user" ? "self-end" : "self-start")} key={idx}>
+                {message.message}
+              </Card>
+            })
+          }
+        </main>
       </aside>
       <div className="h-screen w-full flex flex-col">
         <header className="px-5 h-14 w-full flex items-center gap-5 justify-between bg-secondary-light dark:bg-secondary-dark ">
-          <div className="flex gap-5">
-            <History />
+          <div className="flex gap-5 items-center">
+            <History className="cursor-pointer" onClick={() => setShowHistory(!showHistory)}/>
             <Image src={logo} alt="sixlogo" width={100} />
           </div>
           <ThemeSwitcher />
@@ -35,10 +57,20 @@ export default function Home() {
               <h2>AI Assistant: </h2>
             </CardBody>
           </Card>
-          <form className="w-[95%] self-center my-5 flex gap-5" onSubmit={() => {
+          <form className="w-[95%] self-center my-5 flex gap-5" onSubmit={(e) => {
 
+            e.preventDefault();
+            if (!inputValue) return;
+
+            const currentHistory = history;
+            currentHistory.push({
+              message: inputValue,
+              sender: "user"
+            });
+            setInputValue("");
+            setHistory([...currentHistory]);
           }}>
-            <Input label="prompt your assistant" type="text" />
+            <Input label="Prompt Your Assistant" type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
             <Button color="primary" type="submit" className="h-full">
               Submit
             </Button>
